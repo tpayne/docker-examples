98e3a0575b063b324600d80f68a9e1122b646aa7
Tim Payne <tpayne@users.noreply.github.com>
2021-10-16 09:35:17 +0100

    Update and rename runserver.sh to runTestServer.sh

--- samples/use-cases/postfix-email-relay/runTestServer.sh
+++ samples/use-cases/postfix-email-relay/runTestServer.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# This script is just to build and help test the Postfix config.
+# You will have to deploy the container to K8s as you need
 docker build . -t emailserver:latest -f emailServer.docker --pull
 docker network rm postfixserver
 docker network create --subnet "172.10.0.0/16" --ip-range "172.10.1.0/24" postfixserver
